f248058a92ed717b252d04b50e83c4a604c644ac
Xutainbing <141011184+Xutainbing@users.noreply.github.com>
2025-07-21 18:53:51 +0800

    Update Fig3.R

--- Figure_code/Fig3.R
+++ Figure_code/Fig3.R
@@ -1,27 +1,29 @@
-############# Fig3A - UMAP plot of T/NK subclusters
+############# Fig3A - UMAP plot of B cell subclusters
 
 library(Seurat)
 library(ggplot2)
 
+# Load B cell Seurat object
 sce.Bcell <- readRDS("scRNA-seq/GC_AF/data/sce.Bcell.rds")
 
-my_cols = c('#87cdcb','#d5e9e8','#abaad4','#e7e7f3','#f4b3cb','#96a8d6','#FDB28F','#B49FDC','#FE9392','#3367A2','#FAE28C','#84a3c7','#FDE6D9')
+# Define colors for clusters
+my_cols <- c('#87cdcb','#d5e9e8','#abaad4','#e7e7f3','#f4b3cb','#96a8d6',
+             '#FDB28F','#B49FDC','#FE9392','#3367A2','#FAE28C','#84a3c7','#FDE6D9')
 
-DimPlot(sce.Bcell, group.by = c("CellType"), reduction = "fastMNNUMAP2D", cols = my_cols, pt.size = 1) +
-  labs(x = "UMAP1", y = "UMAP2",title= NULL) +
+# Plot UMAP colored by CellType
+DimPlot(sce.Bcell, group.by = "CellType", cols = my_cols, pt.size = 1) +
+  labs(x = "UMAP1", y = "UMAP2", title = NULL) +
+  theme_minimal(base_size = 12) +
   theme(
-    panel.border = element_rect(fill=NA, color="black", size=2, linetype="solid"),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    text = element_text(size = 20)  # 设置注释字体大小
+    panel.border = element_rect(fill = NA, color = "black", linewidth = 1),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.line = element_blank(),
+    panel.grid = element_blank(),
+    legend.title = element_text(size = 12),
+    legend.text = element_text(size = 11)
   ) +
-    guides(colour = guide_legend(override.aes = list(size = 8)))+
-    annotate(
-        "text", 
-        x = -Inf, y = -Inf,  # 指定左下角位置
-        label = "n = 4237 cells", 
-        hjust = -0.1, vjust = -0.5,  # 调整位置
-        size = 5, color = "black", fontface = "italic"
-    )
+  guides(colour = guide_legend(override.aes = list(size = 8))) +
+  annotate("text", x = -Inf, y = -Inf, label = "n = 4237 cells", 
+           hjust = -0.1, vjust = -0.5, size = 5, color = "black", fontface = "italic")
+
